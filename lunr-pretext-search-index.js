b7b71dea60b785813336f82ec5ac4da14cdff2db
Richard's Math Stuff <richard.zhao@pcc.edu>
2025-12-12 01:12:51 +0000

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,473 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "front-colophon",
+  "level": "1",
+  "url": "front-colophon.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  "
+},
+{
+  "id": "sec121-VectorsPlane",
+  "level": "1",
+  "url": "sec121-VectorsPlane.html",
+  "type": "Section",
+  "number": "12.1",
+  "title": "Vectors in the Plane",
+  "body": " Vectors in the Plane   The big question in this section is how to approximate the area under the curve of a function . We will learn some methods to approximate the area and also how to make the approximation more and more precise.    After this section, students will be able to:     define what a vector is in the plane    perform vector addition and scalar multiplication geometrically and component-wise       Vector Algebra   "
+},
+{
+  "id": "sec121-VectorsPlane-2-2",
+  "level": "2",
+  "url": "sec121-VectorsPlane.html#sec121-VectorsPlane-2-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "  After this section, students will be able to:     define what a vector is in the plane    perform vector addition and scalar multiplication geometrically and component-wise    "
+},
+{
+  "id": "sec122-3DSpaces",
+  "level": "1",
+  "url": "sec122-3DSpaces.html",
+  "type": "Section",
+  "number": "12.2",
+  "title": "Three-Dimensional Spaces: Surfaces, Vectors, and Curves",
+  "body": " Three-Dimensional Spaces: Surfaces, Vectors, and Curves   Just the introduction texts...    After this section, students will be able to:     blahblahblah      "
+},
+{
+  "id": "sec122-3DSpaces-2-2",
+  "level": "2",
+  "url": "sec122-3DSpaces.html#sec122-3DSpaces-2-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "  After this section, students will be able to:     blahblahblah    "
+},
+{
+  "id": "sec123-DotProduct",
+  "level": "1",
+  "url": "sec123-DotProduct.html",
+  "type": "Section",
+  "number": "12.3",
+  "title": "Dot Product and the Angle Between Two Vectors",
+  "body": " Dot Product and the Angle Between Two Vectors   Just the introduction texts...    After this section, students will be able to:     blahblahblah      "
+},
+{
+  "id": "sec123-DotProduct-2-2",
+  "level": "2",
+  "url": "sec123-DotProduct.html#sec123-DotProduct-2-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "  After this section, students will be able to:     blahblahblah    "
+},
+{
+  "id": "sec124-CrossProduct",
+  "level": "1",
+  "url": "sec124-CrossProduct.html",
+  "type": "Section",
+  "number": "12.4",
+  "title": "The Cross Product",
+  "body": " The Cross Product   The big question in this section is how to approximate the area under the curve of a function . We will learn some methods to approximate the area and also how to make the approximation more and more precise.    After this section, students will be able to:     define what a vector is in the plane    perform vector addition and scalar multiplication geometrically and component-wise       The Cross Product   The Right Hand Rule for Cross Products   A diagram showing the right hand rule where the index finger points along vector A, middle finger along B, and thumb illustrates the resulting cross product.     "
+},
+{
+  "id": "sec124-CrossProduct-2-2",
+  "level": "2",
+  "url": "sec124-CrossProduct.html#sec124-CrossProduct-2-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "  After this section, students will be able to:     define what a vector is in the plane    perform vector addition and scalar multiplication geometrically and component-wise    "
+},
+{
+  "id": "fig-cross-product-rhr",
+  "level": "2",
+  "url": "sec124-CrossProduct.html#fig-cross-product-rhr",
+  "type": "Figure",
+  "number": "12.4.1",
+  "title": "",
+  "body": " The Right Hand Rule for Cross Products   A diagram showing the right hand rule where the index finger points along vector A, middle finger along B, and thumb illustrates the resulting cross product.   "
+},
+{
+  "id": "sec125-Planesin3D",
+  "level": "1",
+  "url": "sec125-Planesin3D.html",
+  "type": "Section",
+  "number": "12.5",
+  "title": "Planes in 3-Spaces",
+  "body": " Planes in 3-Spaces   Just the introduction texts...    After this section, students will be able to:     blahblahblah      "
+},
+{
+  "id": "sec125-Planesin3D-2-2",
+  "level": "2",
+  "url": "sec125-Planesin3D.html#sec125-Planesin3D-2-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "  After this section, students will be able to:     blahblahblah    "
+},
+{
+  "id": "sec126-QuadricSurfaces",
+  "level": "1",
+  "url": "sec126-QuadricSurfaces.html",
+  "type": "Section",
+  "number": "12.6",
+  "title": "A Survey to Quadric Surfaces",
+  "body": " A Survey to Quadric Surfaces   Just the introduction texts...    After this section, students will be able to:     blahblahblah      "
+},
+{
+  "id": "sec126-QuadricSurfaces-2-2",
+  "level": "2",
+  "url": "sec126-QuadricSurfaces.html#sec126-QuadricSurfaces-2-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "  After this section, students will be able to:     blahblahblah    "
+},
+{
+  "id": "sec127-CylindricalSphericalCoordinates",
+  "level": "1",
+  "url": "sec127-CylindricalSphericalCoordinates.html",
+  "type": "Section",
+  "number": "12.7",
+  "title": "Cylindrical and Spherical Coordinates",
+  "body": " Cylindrical and Spherical Coordinates   Just the introduction texts...    After this section, students will be able to:     blahblahblah      "
+},
+{
+  "id": "sec127-CylindricalSphericalCoordinates-2-2",
+  "level": "2",
+  "url": "sec127-CylindricalSphericalCoordinates.html#sec127-CylindricalSphericalCoordinates-2-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "  After this section, students will be able to:     blahblahblah    "
+},
+{
+  "id": "sec131-VectorFunctions",
+  "level": "1",
+  "url": "sec131-VectorFunctions.html",
+  "type": "Section",
+  "number": "13.1",
+  "title": "Vector-Valued Functions",
+  "body": " Vector-Valued Functions   Just the introduction texts...    After this section, students will be able to:     blahblahblah      "
+},
+{
+  "id": "sec131-VectorFunctions-2-2",
+  "level": "2",
+  "url": "sec131-VectorFunctions.html#sec131-VectorFunctions-2-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "  After this section, students will be able to:     blahblahblah    "
+},
+{
+  "id": "sec132-CalcVectorFunctions",
+  "level": "1",
+  "url": "sec132-CalcVectorFunctions.html",
+  "type": "Section",
+  "number": "13.2",
+  "title": "Calculus of Vector-Valued Functions",
+  "body": " Calculus of Vector-Valued Functions   Just the introduction texts...    After this section, students will be able to:     blahblahblah      "
+},
+{
+  "id": "sec132-CalcVectorFunctions-2-2",
+  "level": "2",
+  "url": "sec132-CalcVectorFunctions.html#sec132-CalcVectorFunctions-2-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "  After this section, students will be able to:     blahblahblah    "
+},
+{
+  "id": "sec133-ArcLength",
+  "level": "1",
+  "url": "sec133-ArcLength.html",
+  "type": "Section",
+  "number": "13.3",
+  "title": "Arc Length and Speed",
+  "body": " Arc Length and Speed   Just the introduction texts...    After this section, students will be able to:     blahblahblah      "
+},
+{
+  "id": "sec133-ArcLength-2-2",
+  "level": "2",
+  "url": "sec133-ArcLength.html#sec133-ArcLength-2-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "  After this section, students will be able to:     blahblahblah    "
+},
+{
+  "id": "sec134-Curvature",
+  "level": "1",
+  "url": "sec134-Curvature.html",
+  "type": "Section",
+  "number": "13.4",
+  "title": "Curvature",
+  "body": " Curvature   Just the introduction texts...    After this section, students will be able to:     blahblahblah      "
+},
+{
+  "id": "sec134-Curvature-2-2",
+  "level": "2",
+  "url": "sec134-Curvature.html#sec134-Curvature-2-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "  After this section, students will be able to:     blahblahblah    "
+},
+{
+  "id": "sec141-FunctionsTwoVariables",
+  "level": "1",
+  "url": "sec141-FunctionsTwoVariables.html",
+  "type": "Section",
+  "number": "14.1",
+  "title": "Functions of Two or More Variables",
+  "body": " Functions of Two or More Variables   Just the introduction texts...    After this section, students will be able to:     blahblahblah      "
+},
+{
+  "id": "sec141-FunctionsTwoVariables-2-2",
+  "level": "2",
+  "url": "sec141-FunctionsTwoVariables.html#sec141-FunctionsTwoVariables-2-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "  After this section, students will be able to:     blahblahblah    "
+},
+{
+  "id": "sec142-LimitsContinuity",
+  "level": "1",
+  "url": "sec142-LimitsContinuity.html",
+  "type": "Section",
+  "number": "14.2",
+  "title": "Limits and Continuity in Several Variables",
+  "body": " Limits and Continuity in Several Variables   Just the introduction texts...    After this section, students will be able to:     blahblahblah      "
+},
+{
+  "id": "sec142-LimitsContinuity-2-2",
+  "level": "2",
+  "url": "sec142-LimitsContinuity.html#sec142-LimitsContinuity-2-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "  After this section, students will be able to:     blahblahblah    "
+},
+{
+  "id": "sec143-PartialDerivatives",
+  "level": "1",
+  "url": "sec143-PartialDerivatives.html",
+  "type": "Section",
+  "number": "14.3",
+  "title": "Partial Derivatives",
+  "body": " Partial Derivatives   Just the introduction texts...    After this section, students will be able to:     blahblahblah      "
+},
+{
+  "id": "sec143-PartialDerivatives-2-2",
+  "level": "2",
+  "url": "sec143-PartialDerivatives.html#sec143-PartialDerivatives-2-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "  After this section, students will be able to:     blahblahblah    "
+},
+{
+  "id": "sec144-DifferentiabilityTangentPlane",
+  "level": "1",
+  "url": "sec144-DifferentiabilityTangentPlane.html",
+  "type": "Section",
+  "number": "14.4",
+  "title": "Differentiability, Tangent Planes, and Linear Approximation",
+  "body": " Differentiability, Tangent Planes, and Linear Approximation   Just the introduction texts...    After this section, students will be able to:     blahblahblah      "
+},
+{
+  "id": "sec144-DifferentiabilityTangentPlane-2-2",
+  "level": "2",
+  "url": "sec144-DifferentiabilityTangentPlane.html#sec144-DifferentiabilityTangentPlane-2-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "  After this section, students will be able to:     blahblahblah    "
+},
+{
+  "id": "sec145-GradientDirectionalDerivatives",
+  "level": "1",
+  "url": "sec145-GradientDirectionalDerivatives.html",
+  "type": "Section",
+  "number": "14.5",
+  "title": "The Gradient and Directional Derivatives",
+  "body": " The Gradient and Directional Derivatives   Just the introduction texts...    After this section, students will be able to:     blahblahblah      "
+},
+{
+  "id": "sec145-GradientDirectionalDerivatives-2-2",
+  "level": "2",
+  "url": "sec145-GradientDirectionalDerivatives.html#sec145-GradientDirectionalDerivatives-2-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "  After this section, students will be able to:     blahblahblah    "
+},
+{
+  "id": "sec146-ChainRule",
+  "level": "1",
+  "url": "sec146-ChainRule.html",
+  "type": "Section",
+  "number": "14.6",
+  "title": "Multivariable Calculus Chain Rules",
+  "body": " Multivariable Calculus Chain Rules   Just the introduction texts...    After this section, students will be able to:     blahblahblah      "
+},
+{
+  "id": "sec146-ChainRule-2-2",
+  "level": "2",
+  "url": "sec146-ChainRule.html#sec146-ChainRule-2-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "  After this section, students will be able to:     blahblahblah    "
+},
+{
+  "id": "sec147-Optimization",
+  "level": "1",
+  "url": "sec147-Optimization.html",
+  "type": "Section",
+  "number": "14.7",
+  "title": "Optimization in Several Variables",
+  "body": " Optimization in Several Variables   Just the introduction texts...    After this section, students will be able to:     blahblahblah      "
+},
+{
+  "id": "sec147-Optimization-2-2",
+  "level": "2",
+  "url": "sec147-Optimization.html#sec147-Optimization-2-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "  After this section, students will be able to:     blahblahblah    "
+},
+{
+  "id": "sec148-LagrangeMultipliers",
+  "level": "1",
+  "url": "sec148-LagrangeMultipliers.html",
+  "type": "Section",
+  "number": "14.8",
+  "title": "Lagrange Multipliers: Optimizing with a Constraint",
+  "body": " Lagrange Multipliers: Optimizing with a Constraint   Just the introduction texts...    After this section, students will be able to:     blahblahblah      "
+},
+{
+  "id": "sec148-LagrangeMultipliers-2-2",
+  "level": "2",
+  "url": "sec148-LagrangeMultipliers.html#sec148-LagrangeMultipliers-2-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "  After this section, students will be able to:     blahblahblah    "
+},
+{
+  "id": "sec151-IntegrationTwoVariables",
+  "level": "1",
+  "url": "sec151-IntegrationTwoVariables.html",
+  "type": "Section",
+  "number": "15.1",
+  "title": "Integration in Two Variables",
+  "body": " Integration in Two Variables   Just the introduction texts...    After this section, students will be able to:     blahblahblah      "
+},
+{
+  "id": "sec151-IntegrationTwoVariables-2-2",
+  "level": "2",
+  "url": "sec151-IntegrationTwoVariables.html#sec151-IntegrationTwoVariables-2-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "  After this section, students will be able to:     blahblahblah    "
+},
+{
+  "id": "sec152-DoubleIntegrals",
+  "level": "1",
+  "url": "sec152-DoubleIntegrals.html",
+  "type": "Section",
+  "number": "15.2",
+  "title": "Double Integrals over More General Regions",
+  "body": " Double Integrals over More General Regions   Just the introduction texts...    After this section, students will be able to:     blahblahblah      "
+},
+{
+  "id": "sec152-DoubleIntegrals-2-2",
+  "level": "2",
+  "url": "sec152-DoubleIntegrals.html#sec152-DoubleIntegrals-2-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "  After this section, students will be able to:     blahblahblah    "
+},
+{
+  "id": "sec153-tripleIntegrals",
+  "level": "1",
+  "url": "sec153-tripleIntegrals.html",
+  "type": "Section",
+  "number": "15.3",
+  "title": "Triple Integrals",
+  "body": " Triple Integrals   Just the introduction texts...    After this section, students will be able to:     blahblahblah      "
+},
+{
+  "id": "sec153-tripleIntegrals-2-2",
+  "level": "2",
+  "url": "sec153-tripleIntegrals.html#sec153-tripleIntegrals-2-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "  After this section, students will be able to:     blahblahblah    "
+},
+{
+  "id": "sec154-IntegrationPolarCylindricalSphericalCoordinates",
+  "level": "1",
+  "url": "sec154-IntegrationPolarCylindricalSphericalCoordinates.html",
+  "type": "Section",
+  "number": "15.4",
+  "title": "Integration in Polar, Cylindrical, and Spherical Coordinates",
+  "body": " Integration in Polar, Cylindrical, and Spherical Coordinates   Just the introduction texts...    After this section, students will be able to:     blahblahblah      "
+},
+{
+  "id": "sec154-IntegrationPolarCylindricalSphericalCoordinates-2-2",
+  "level": "2",
+  "url": "sec154-IntegrationPolarCylindricalSphericalCoordinates.html#sec154-IntegrationPolarCylindricalSphericalCoordinates-2-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "  After this section, students will be able to:     blahblahblah    "
+},
+{
+  "id": "sec156-ChangeofVariables",
+  "level": "1",
+  "url": "sec156-ChangeofVariables.html",
+  "type": "Section",
+  "number": "15.5",
+  "title": "Change of Variables",
+  "body": " Change of Variables   Just the introduction texts...    After this section, students will be able to:     blahblahblah      "
+},
+{
+  "id": "sec156-ChangeofVariables-2-2",
+  "level": "2",
+  "url": "sec156-ChangeofVariables.html#sec156-ChangeofVariables-2-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "  After this section, students will be able to:     blahblahblah    "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
